92ce191d1dabc33912565171c4b4af60789d81fb
Hack Reactor Students <accounts@hackreactor.com>
2016-04-07 15:40:05 -0700

    modify search controller, service, and html

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,7 @@
-(function() { 
 
-  // <script type="text/javascript" src="../node_modules/jquery/dist/jquery.min.js"></script>
-  // <script src="../node_modules/angular/angular.min.js"></script>
-  // <script src="../node_modules/angular/angular-route.min.js"></script>
-  // <script src="js/app.js"></script>
-  // <script src="js/search/search.service.js"></script>
-  // <script src="js/search/search.controller.js"></script>
-
-
+(function() {
 
 'use strict';
-
-// Since we're using a webpack, we have to require dependencies
-// in each module. Webpacks make each file its own module
-require('jquery');
-require('angular');
-require('angular-route');
 
 // Declare app level module which depends on filters, and services
 
@@ -23,22 +9,15 @@ require('angular-route');
   .config(function ($routeProvider, $locationProvider) {
     $routeProvider
       .when('/', {
-        // The difference between templateUrl and template
-        // is that templateUrl requires a path, whereas template
-        // requires a string. By requiring the file, we are essentially
-        // piping the entire file as a string to angular's template method
-        // We need a string, because this webpack cannot load files. It loads
-        // reads everything as a string, then it bundles everything as a string.
-        template: require('./search/search.html'),
+        templateUrl: 'search/search.html',
         controller: 'SearchController'
       })
       .otherwise({
-        redirectTo: '/'
-      });
+        redirectTo: '/',
+      })
+
 
     $locationProvider.html5Mode(true);
   });
 
-  // Load modules here. This must come after the configuration
-  require('./search');
 })();
