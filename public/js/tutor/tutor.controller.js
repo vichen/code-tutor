15809297bb1b49e0ca8434e509d1da4be59d53e5
Hack Reactor Students <accounts@hackreactor.com>
2016-04-07 11:54:54 -0700

    add tutor controller and service

---
+++ public/js/tutor/tutor.controller.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('myApp')
+
+.controller('tutorController', function ($scope, $http) {
+
+    $http({
+      method: 'GET',
+      url: '/api/name'
+    }).
+    success(function (data, status, headers, config) {
+      $scope.name = data.name;
+    }).
+    error(function (data, status, headers, config) {
+      $scope.name = 'Error!';
+    });
+
+});
