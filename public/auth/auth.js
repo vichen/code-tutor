6ea01cd2d18fb5763cd9158c39a753cf620e2fac
Vivian Chen <vivian.vc.chen@gmail.com>
2016-04-06 20:03:35 -0700

    begin implementing auth controller

--- public/auth/auth.js
+++ public/auth/auth.js
@@ -4,10 +4,13 @@ angular.module('llama.auth', [])
   $scope.user = {};
 
   $scope.signin = function () {
+
+    // TODO: define an auth service in services folder.
     Auth.signin($scope.user)
       .then(function (token) {
 
         // TODO: understand what this line of code is doing
+        // find a way to store into the database
         $window.localStorage.setItem('com.llama', token);
 
         //once the user signs in, we want them to be able to view their own home page
