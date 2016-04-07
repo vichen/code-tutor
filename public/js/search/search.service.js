108e0f202640c49e0d55d504d8188a0cc46b36de
Hack Reactor Students <accounts@hackreactor.com>
2016-04-07 11:54:39 -0700

    add search controller and service

--- public/js/search/search.service.js
+++ public/js/search/search.service.js
@@ -1,3 +1,4 @@
+<<<<<<< 0a5e91da556cdff90d3704475a93189c44aabc55
 (function() {
   
   'use strict';
@@ -21,3 +22,23 @@
   });
 })();
 
+=======
+'use strict';
+
+/* Services */
+
+angular.module('myApp')
+
+.service('SearchService', function($http) {
+
+  // TODO: figure out which query to search by
+  this.getTutors = function(query) {
+    // TODO: make sure to get api url from server side
+    return $http.get(url)
+      .then(function(res) {
+        return res.data.tutors;
+      })
+  }
+});
+  
+>>>>>>> add search controller and service
