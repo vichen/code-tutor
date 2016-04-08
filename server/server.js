6b61ce25f81009d27b42232605738efbe108d260
Roland Fung <roland.c.fung@gmail.com>
2016-04-08 01:35:19 -0700

    small fixes

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(bodyParser.json());
-app.use(methodOverride());
+app.use(methodOverride()); 
 
 
 var isProduction = process.env.NODE_ENV === 'production';
@@ -26,26 +26,8 @@ var publicPath = path.resolve(__dirname, '..', 'public');
 //set up routes here from routes file
 require('./routes')(app, express);
 
-app.listen(port, function () {
-});
+app.listen(port);
 
-
-// var express = require('express');
-// var mongoose = require('mongoose');
-
-// var app = express();
-
-// // connect to mongo database named "shortly"
-// mongoose.connect('mongodb://localhost/shortly');
-
-// // configure our server with all the middleware and routing
-// require('./config/middleware.js')(app, express);
-// require('./config/routes.js')(app, express);
-
-// // start listening to requests on port 8000
-// app.listen(8000);
-
-// // export our app for testing and flexibility, required by index.js
 module.exports = app;
 
 
