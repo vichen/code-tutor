62ae6663d1dff3fcc35cba1895af69683fbf490f
Vivian Chen <vivian.vc.chen@gmail.com>
2016-04-07 23:43:43 -0700

    fix merge conflict

--- public/js/search/search.controller.js
+++ public/js/search/search.controller.js
@@ -10,7 +10,6 @@
 
     // initialize empty tutor data array that will hold search results
     $scope.tutorData = [];
-<<<<<<< bfea58b112448ce32665b81f8a864aa68436cc9b
 
     // define search on scope
     $scope.search = function(query) {
@@ -32,27 +31,3 @@
 
   });
 })();
-=======
-
-    // define search on scope
-    $scope.search = function(query) {
-
-      // call function from SearchService
-      SearchService.getTutors(query)
-
-        // upon success, assign returned tutors data to scope's tutorData
-        .then(function(tutors) {
-          $scope.tutorData = tutors;
-        })
-
-        // on error, console log error
-        .catch(function(error) {
-          console.log('There was an error retrieving tutor data: ', error);
-        })
-    }
-     
-
-  });
-})();
-
->>>>>>> modify search controller, service, and html
