7fa8e51707850b4f4a50d3f723fc29b012130478
Vivian Chen <vivian.vc.chen@gmail.com>
2016-04-08 08:59:06 -0700

    fix merge conflict

--- public/js/search/search.service.js
+++ public/js/search/search.service.js
@@ -1,7 +1,3 @@
-<<<<<<< bfea58b112448ce32665b81f8a864aa68436cc9b
-<<<<<<< 0a5e91da556cdff90d3704475a93189c44aabc55
-=======
->>>>>>> modify search controller, service, and html
 (function() {
   
   'use strict';
@@ -9,7 +5,6 @@
   /* Services */
 
   angular.module('myApp')
-<<<<<<< bfea58b112448ce32665b81f8a864aa68436cc9b
 
   .service('SearchService', function($http) {
 
@@ -26,25 +21,3 @@
   });
 })();
 
-=======
-'use strict';
-=======
->>>>>>> modify search controller, service, and html
-
-  .service('SearchService', function($http) {
-
-    // TODO: figure out which query to search by
-    this.getTutors = function(query) {
-
-      // TODO: make sure to get api url from server side
-      // make GET request to api (db) to get tutor data array
-      return $http.get(url)
-        .then(function(res) {
-          return res.data.tutors;
-        })
-    }
-  });
-})();
-
-  
->>>>>>> add search controller and service
