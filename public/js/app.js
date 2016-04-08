a71f0ff6add907a1fffb100cf1be79f2f73e3e22
Vivian Chen <vivian.vc.chen@gmail.com>
2016-04-07 23:14:00 -0700

    front end appears in browser

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,5 @@
 (function() { 
 
-  // import angular from 'angular'
-  // import 
-
-  // var angular = require('angular')
-
-
   // <script type="text/javascript" src="../node_modules/jquery/dist/jquery.min.js"></script>
   // <script src="../node_modules/angular/angular.min.js"></script>
   // <script src="../node_modules/angular/angular-route.min.js"></script>
@@ -17,28 +11,34 @@
 
 'use strict';
 
+// Since we're using a webpack, we have to require dependencies
+// in each module. Webpacks make each file its own module
+require('jquery');
+require('angular');
+require('angular-route');
+
 // Declare app level module which depends on filters, and services
 
   angular.module('myApp', ['ngRoute'])
   .config(function ($routeProvider, $locationProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'search/search.html',
+        // The difference between templateUrl and template
+        // is that templateUrl requires a path, whereas template
+        // requires a string. By requiring the file, we are essentially
+        // piping the entire file as a string to angular's template method
+        // We need a string, because this webpack cannot load files. It loads
+        // reads everything as a string, then it bundles everything as a string.
+        template: require('./search/search.html'),
         controller: 'SearchController'
       })
-
-      .when('/view1', {
-        templateUrl: 'partials/partial1',
-        controller: 'MyCtrl1'
-      })
-      .when('/view2', {
-        templateUrl: 'partials/partial2',
-        controller: 'MyCtrl2'
-      })
       .otherwise({
-        redirectTo: '/view1'
+        redirectTo: '/'
       });
 
     $locationProvider.html5Mode(true);
   });
+
+  // Load modules here. This must come after the configuration
+  require('./search');
 })();
