930e21e18538a2df956f8a0f7091f870fbfe7af3
adamrgisom <adamrgisom@gmail.com>
2016-04-12 16:12:36 -0700

    Get auth controller signin http request to work

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -1,6 +1,30 @@
 
 angular.module('codellama.auth', [])
 
-.controller('AuthController', function($scope) {
+.controller('AuthController', function($scope, $window, $location, $http) {
   $scope.dummyData = ['hi', 'I am', 'Fred'];
+  // we want form submit to ... connect to backend ... with email & password
+  //
+  $scope.user = {};
+  var user = $scope.user;
+
+  $scope.signin = function() {
+    return $http({
+      method: 'POST',
+      url: '/api/users/signin/',
+      data: $scope.user
+    })
+    .then(function (res) {
+      $window.localStorage.setItem('com.codeLlama', res.data.token);
+      console.dir($window.localStorage);
+      $location.path('/'); // do we want it to redirect to home?
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+
+
+  };
+
+
 });
