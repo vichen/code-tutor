6c869fb78fa0722cc72766732fd80f215e2e1e3d
Roland Fung <roland.c.fung@gmail.com>
2016-04-13 14:02:49 -0700

    image wip

--- client/app/profile/profile.js
+++ client/app/profile/profile.js
@@ -2,21 +2,23 @@
 var app = angular.module('codellama.fileUpload', ['ngFileUpload']);
 
 app.controller('uploadCtrl', ['$scope', 'Upload', '$timeout', function ($scope, Upload, $timeout) {
-    $scope.upload = function (dataUrl, name) {
-        Upload.upload({
-            url: 'api/users/profilePic',
-            data: {
-                file: Upload.dataUrltoBlob(dataUrl, name)
-            },
-        }).then(function (response) {
-            $timeout(function () {
-                $scope.result = response.data;
-            });
-        }, function (response) {
-            if (response.status > 0) $scope.errorMsg = response.status 
-                + ': ' + response.data;
-        }, function (evt) {
-            $scope.progress = parseInt(100.0 * evt.loaded / evt.total);
-        });
-    }
+  $scope.upload = function (dataUrl, name) {
+    Upload.upload({
+      url: 'api/users/profilePic',
+      data: {
+        file: Upload.dataUrltoBlob(dataUrl, name)
+      },
+    }).then(function (response) {
+      $timeout(function () {
+        $scope.result = response.data;
+      });
+    }, function (response) {
+      if (response.status > 0) {
+        $scope.errorMsg = response.status 
+        + ': ' + response.data;
+      }
+    }, function (evt) {
+      $scope.progress = parseInt(100.0 * evt.loaded / evt.total);
+    });
+  };
 }]);
