fde2e4fa9406c6cfae30176abae498017b687929
adamrgisom <adamrgisom@gmail.com>
2016-04-14 13:10:29 -0700

    Start work on making nav bar hide/show based on loggedIn state

--- client/app/nav/nav.js
+++ client/app/nav/nav.js
@@ -1,5 +1,8 @@
-angular.module('codellama.nav', [])
+angular.module('codellama.nav', ['Auth'])
 
-.controller('NavController', function($scope) {
+.controller('NavController', function($scope, Auth) {
   $scope.data = 3;
+  $scope.loggedIn = function() {
+    return Auth.isAuth();
+  };
 });
