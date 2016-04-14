75c5672c9aa0fde3e49d752f4f867ee7cb8c7825
adamrgisom <adamrgisom@gmail.com>
2016-04-14 16:35:08 -0700

    Create a nav bar that changes depending on if a user is logged in & a tutor

--- client/app/app.js
+++ client/app/app.js
@@ -69,6 +69,11 @@
       return attach;
     })
     .run(function ($rootScope, $location, Auth) {
+
+      $rootScope.loggedIn = Auth.isLoggedIn();
+      $rootScope.loggedInAndTutor = Auth.isLoggedInAndTutor();
+      $rootScope.loggedInNotTutor = Auth.isLoggedInButNotTutor();
+
       // here inside the run phase of angular, our services and controllers
       // have just been registered and our app is ready
       // however, we want to make sure the user is authorized
@@ -76,9 +81,9 @@
       // when it does change routes, we then look for the token in localstorage
       // and send that token to the server to see if it is a real user or hasn't expired
       // if it's not valid, we then redirect back to signin/signup
-      $rootScope.$on('$routeChangeStart', function (evt, next, current) {
-        if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
-          $location.path('/signin');
-        }
-      });
+      // $rootScope.$on('$routeChangeStart', function (evt, next, current) {
+      //   if (next.$$route && next.$$route.authenticate && !Auth.isLoggedIn()) {
+      //     $location.path('/signin');
+      //   }
+      // });
     });
