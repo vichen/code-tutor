5eb25e20332f8acfdee604cbf94393aa8abadb1d
Roland Fung <roland.c.fung@gmail.com>
2016-04-14 15:00:15 -0700

    add cities drodown, saves to db

--- client/app/profile/update.js
+++ client/app/profile/update.js
@@ -2,22 +2,38 @@
 var app = angular.module('codellama.fileUpload', ['ngFileUpload', 'checklist-model']);
 
 app.controller('uploadCtrl', ['$scope', 'Upload', '$timeout', function ($scope, Upload, $timeout) {
-  $scope.roles = [
+  $scope.data = {};
+  $scope.data.subjects = [];
+  $scope.data.location = {};
+
+  $scope.subjects = [
     'javascript', 
     'c', 
     'python', 
     'ruby'
   ];
-  $scope.user = {
-    roles: []
-  };
+  // $scope.user = {
+  //   roles: []
+  // };
+
+  $scope.cities = [
+    'San Francisco',
+    'San Jose',
+    'San Mateo',
+    'Cupertino',
+    'Mountain View',
+    'Sunnyvale',
+    'Berkeley',
+    'Oakland'
+  ];
 
   $scope.uploadPic = function(file) {
     if (!file) { var file = {}; }
     
     file.upload = Upload.upload({
       url: 'api/users/profile',
-      data: {name: $scope.name, email: $scope.email, bio: $scope.bio, file: file, isTutor: $scope.isTutor, subjects: $scope.user.roles}
+      // data: {name: $scope.name, city: $scope.city, email: $scope.email, bio: $scope.bio, file: file, isTutor: $scope.isTutor, subjects: $scope.user.roles}
+      data: $scope.data
     });
 
     file.upload.then(function (response) {
