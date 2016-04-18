d84b362b678c893fdf3d4afca4d4876e6bd79dd6
Roland Fung <roland.c.fung@gmail.com>
2016-04-18 00:58:22 -0700

    remove duplicate navbar, fix active class

--- client/app/nav/nav.js
+++ client/app/nav/nav.js
@@ -1,5 +1,10 @@
 angular.module('codellama.nav', [])
 
-.controller('NavController', function($scope, $rootScope, Auth) {
+.controller('NavController', function($scope, $rootScope, Auth, $location) {
+
+  // adds bootstrap active class if path matches href value
+  $scope.isActive = function (viewLocation) { 
+    return viewLocation === $location.path();
+  };
 
 });
